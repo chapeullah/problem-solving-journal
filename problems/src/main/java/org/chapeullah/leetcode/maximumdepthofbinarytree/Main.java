5325df070553d2916d39ce83b0cc158e295350c6
chapeullah <chapeullah@gmail.com>
2026-08-20 06:45:45 +0300

    Implement maxDepth and add demo Main

--- problems/src/main/java/org/chapeullah/leetcode/maximumdepthofbinarytree/Main.java
+++ problems/src/main/java/org/chapeullah/leetcode/maximumdepthofbinarytree/Main.java
@@ -2,6 +2,14 @@ package org.chapeullah.leetcode.maximumdepthofbinarytree;
 
 public class Main {
     public static void main(String[] args) {
-
+        TreeNode root = new TreeNode(
+                3,
+                new TreeNode(9),
+                new TreeNode(
+                        20,
+                        new TreeNode(15),
+                        new TreeNode(7)));
+        Solution solution = new Solution();
+        System.out.println(solution.maxDepth(root));
     }
 }
--- problems/src/main/java/org/chapeullah/leetcode/maximumdepthofbinarytree/Solution.java
+++ problems/src/main/java/org/chapeullah/leetcode/maximumdepthofbinarytree/Solution.java
@@ -2,10 +2,17 @@ package org.chapeullah.leetcode.maximumdepthofbinarytree;
 
 class Solution {
     public int maxDepth(TreeNode root) {
-        return -1;
+        return recursive(root);
     }
 
-    private void recursive() {
-        
+    private int recursive(TreeNode node) {
+        if (node == null) return 0;
+
+        System.out.println(node.val);
+
+        int leftDepth = recursive(node.left);
+        int rightDepth = recursive(node.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
     }
 }
